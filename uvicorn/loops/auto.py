e24834dd3145e76e74d298b4bb147ff0e8e007ce
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-25 23:04:56 -0400

    RabeyaMuna__diff__id_307

--- uvicorn/loops/auto.py
+++ uvicorn/loops/auto.py
@@ -8,9 +8,9 @@ from collections.abc import Callable
 def auto_loop_factory(use_subprocess: bool = False) -> Callable[[], asyncio.AbstractEventLoop]:
     try:
         if sys.platform == "win32":  # pragma: py-win32
-            from uvicorn.loops.winloop import winloop_loop_factory as loop_factory
-        else:  # pragma: py-not-win32
-            from uvicorn.loops.uvloop import uvloop_loop_factory as loop_factory
+            from uvicorn.loops.winloop import winloop_loop_factory as loop_factory  # pragma: py-not-win32
+        else:  # pragma: py-win32
+            from uvicorn.loops.uvloop import uvloop_loop_factory as loop_factory  # pragma: py-win32
     except ImportError:  # pragma: no cover
         from uvicorn.loops.asyncio import asyncio_loop_factory as loop_factory
     return loop_factory(use_subprocess=use_subprocess)
